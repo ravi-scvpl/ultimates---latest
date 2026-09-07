a9f1d0b673e182e55e179d8086fdddd5ddbb571d
8409251542 <ravi.yoof@gmail.com>
2026-09-07 11:48:01 +0530

    improved catlog page style

---
+++ assets/m2m-config.js
@@ -0,0 +1,74 @@
+window.M2M_CONFIG = {
+  "ultiscrub-carpet-mat-1": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 192,
+    "widths": [24, 36, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150, 156, 162, 168, 174, 180, 186, 192]
+  },
+  "ultiscrape-carpet-mat": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 180,
+    "widths": [24, 36, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150, 156, 162, 168, 174, 180]
+  },
+  "ulticlean-carpet-mat-1": {
+    "minWidth": 24,
+    "maxWidth": 36,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 36],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  },
+  "ultimat-heavy-duty-vinyl-looped-matting": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  },
+  "ultirake-carpet-mat": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 36, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  },
+  "ultizig-barrier-mat": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  },
+  "ultizig-barrier-mat-with-backing": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  },
+  "ultidure-carpet-mat": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 96,
+    "widths": [24, 36, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96]
+  },
+  "ultiwipe-carpet-mat": {
+    "minWidth": 24,
+    "maxWidth": 48,
+    "minLength": 48,
+    "maxLength": 144,
+    "widths": [24, 36, 48],
+    "lengths": [48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144]
+  }
+};
